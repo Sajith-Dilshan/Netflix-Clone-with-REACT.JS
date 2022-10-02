94aa819140a7cfadb130babad9a3189430958fc2
sajith <sajithd784@gmail.com>
2022-10-02 13:04:07 +0530

    finalize initial setup and navbar component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBSwT8wckPzHrlFvqOy2vVzuksVGeOApzY",
+    authDomain: "netflix-clone-855e0.firebaseapp.com",
+    projectId: "netflix-clone-855e0",
+    storageBucket: "netflix-clone-855e0.appspot.com",
+    messagingSenderId: "86552851865",
+    appId: "1:86552851865:web:f7b8899c2271ce684a4f08"
+  };
