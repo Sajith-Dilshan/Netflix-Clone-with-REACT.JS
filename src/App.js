93f050f5243e43f9f84b587fb78e5d3ae9db3c6b
sajith <sajithd784@gmail.com>
2022-10-04 20:04:52 +0530

    Build the Row Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
-import Banner from './Banner';
+
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       
       <HomeScreen />
 
-      <Banner />
+      
 
     </div>
   );
--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,7 @@ const API_KEY = "d948ed1710db460b5b94c91138535c47";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/discover/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
