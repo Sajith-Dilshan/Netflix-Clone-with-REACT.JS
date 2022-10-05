06ee9cce7c5450327cebd71a4d81282fb749e469
sajith <sajithd784@gmail.com>
2022-10-05 08:25:01 +0530

    restructure homescreencomponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screen/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
